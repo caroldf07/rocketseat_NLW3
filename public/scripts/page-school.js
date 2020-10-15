386c6362d6c68abeaf193911175b3c2d06480033
caroldf07 <carolina.df@outlook.com>
2020-10-15 11:10:43 -0300

    conclusao do dia 02

---
+++ public/scripts/page-school.js
@@ -0,0 +1,51 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scroolWheelZoom: false,
+    zoomControl: false
+}
+
+//create map
+const map = L.map("mapid", options).setView([-25.4906671,-50.6550479], 16);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/pakua-padrao1.png",
+    iconSize: [58,58],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+
+//create and add marker
+L.marker([-25.4906671,-50.6550479],{ icon })
+    .addTo(map)
+    .openPopup();
+
+//image gallery
+
+function selectImage(event){
+    const button = event.currentTarget
+
+    //remove all classes active
+
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach((button) => {
+        button.classList.remove("active")
+    })
+
+    //select the image clicked
+
+    const image = button.children[0]
+    const imageContainer = document.querySelector("#school-details > img")
+
+    //refresh container image
+    imageContainer.src = image.src
+
+    //add class active
+    button.classList.add('active')
+}
